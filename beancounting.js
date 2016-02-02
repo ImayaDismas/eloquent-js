1870f8360a88229668e2d6b78cca8aeba3900e2b
ImayaDismas <imayadismas@gmail.com>
2016-02-02 20:43:04 +0300

    Add html file running the scripts

---
+++ beancounting.js
@@ -0,0 +1,6 @@
+// Your code here.
+
+console.log(Object.countBs("BBC"));
+// → 2
+console.log(Object.countChar("kakkerlak", "k"));
+// → 4
---
+++ recursion.js
@@ -0,0 +1,8 @@
+// Your code here.
+
+console.log(Object.isEven(50));
+// → true
+console.log(Object.isEven(75));
+// → false
+console.log(Object.isEven(-1));
+// → ??
